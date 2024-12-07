b00dfb0c037b1de2d859b0c5bfe56a98141c1656
Thierry Lachance <lachancethierry6@gmail.com>
2024-12-06 19:13:21 -0500

    code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/score1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/score1.java
@@ -1,19 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 
 @Autonomous
-public class ObservationZone extends LinearOpMode {
+public class score1 extends LinearOpMode {
 
     DcMotor frontLeftMotor;
     DcMotor backLeftMotor;
     DcMotor frontRightMotor;
     DcMotor backRightMotor;
+    DcMotor elevatorMotor;
+
+    CRServo servoBucket;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -21,6 +26,12 @@ public class ObservationZone extends LinearOpMode {
         backLeftMotor = hardwareMap.dcMotor.get("backLeft");
         frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         backRightMotor = hardwareMap.dcMotor.get("backRight");
+        elevatorMotor = hardwareMap.dcMotor.get("elevator");
+        servoBucket = hardwareMap.get(CRServo.class, "servoBucket");
+
+
+        DigitalChannel elevatorIn = hardwareMap.digitalChannel.get("elevatorIn");
+        DigitalChannel elevatorOut = hardwareMap.digitalChannel.get("elevatorOut");
 
 
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -34,16 +45,47 @@ public class ObservationZone extends LinearOpMode {
         backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        elevatorMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elevatorMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
         while (opModeIsActive()) {
-            double targetFL = DrivetrainFunction.calculateTicks(60.0);
-            double targetFR = DrivetrainFunction.calculateTicks(60.0);
-            double targetBL = DrivetrainFunction.calculateTicks(60.0);
-            double targetBR = DrivetrainFunction.calculateTicks(60.0);
+            double targetFL = DrivetrainFunction.calculateTicks(-45.0);
+            double targetFR = DrivetrainFunction.calculateTicks(-45.0);
+            double targetBL = DrivetrainFunction.calculateTicks(-45.0);
+            double targetBR = DrivetrainFunction.calculateTicks(-45.0);
             encoderDrive(targetFL, targetFR, targetBL, targetBR);
 
+            targetFL = frontLeftMotor.getCurrentPosition() + DrivetrainFunction.calculateTicks(-30.0);
+            targetFR = frontRightMotor.getCurrentPosition() + DrivetrainFunction.calculateTicks(30.0);
+            targetBL = backLeftMotor.getCurrentPosition() + DrivetrainFunction.calculateTicks(-30.0);
+            targetBR =  backRightMotor.getCurrentPosition() + DrivetrainFunction.calculateTicks(30.0);
+            encoderDrive(targetFL, targetFR, targetBL, targetBR);
+            sleep(300);
+            elevatorMotor.setPower(ElevatorFunction.moveElevator(0.75, elevatorIn, elevatorOut));
+
+            targetFL = frontLeftMotor.getCurrentPosition() + DrivetrainFunction.calculateTicks(35.0);
+            targetFR = frontRightMotor.getCurrentPosition() + DrivetrainFunction.calculateTicks(35.0);
+            targetBL = backLeftMotor.getCurrentPosition() + DrivetrainFunction.calculateTicks(35.0);
+            targetBR =  backRightMotor.getCurrentPosition() +DrivetrainFunction.calculateTicks(35.0);
+            encoderDrive(targetFL, targetFR, targetBL, targetBR);
+            sleep(600);
+            elevatorMotor.setPower(0);
+            frontLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            backLeftMotor.setPower(0);
+            backRightMotor.setPower(0);
+            servoBucket.setPower(-1);
+            sleep(1800);
+
+            servoBucket.setPower(1);
+            sleep(500);
+            elevatorMotor.setPower(ElevatorFunction.moveElevator(-0.75, elevatorIn, elevatorOut));
+            sleep(500);
+            servoBucket.setPower(0);
+            sleep(300);
+            elevatorMotor.setPower(0);
             sleep(100000);
 
         }
