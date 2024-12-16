3d7f4347342568cfdfd6dafbe14d99b77fb0f663
Thierry Lachance <lachancethierry6@gmail.com>
2024-12-16 14:15:21 -0500

    fuck

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTemplate.java
@@ -6,6 +6,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.teamcode.*;
+
+
+
 
 @Disabled
 @Autonomous
