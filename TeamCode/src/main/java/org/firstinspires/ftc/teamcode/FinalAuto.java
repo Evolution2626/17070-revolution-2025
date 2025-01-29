b025a45d19586a0343619ddc70f1c9ab289fe106
Thierry Lachance <lachancethierry6@gmail.com>
2025-01-29 08:46:14 -0500

    it work!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalAuto.java
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
 import java.util.Locale;
 
-@Autonomous(name = "it's the final auto tututududu", group = "Pinpoint")
+@Autonomous(name = "it's the final auto tututududu")
 //@Disabled
 
 public class FinalAuto extends LinearOpMode {
@@ -46,15 +46,27 @@ public class FinalAuto extends LinearOpMode {
         DRIVE_TO_TARGET_1,
         DRIVE_TO_TARGET_2,
         DRIVE_TO_TARGET_3,
+        Drive_TO_TARGET_3_1,
         DRIVE_TO_TARGET_4,
-        DRIVE_TO_TARGET_5
+        DRIVE_TO_TARGET_5,
+        DRIVE_TO_TARGET_5_1,
+        DRIVE_TO_TARGET_6,
+        DRIVE_TO_TARGET_7,
+        Drive_TO_TARGET_7_1,
+        DRIVE_TO_TARGET_8
     }
 
-    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM, 200, 0, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM, 200, 0, AngleUnit.DEGREES, -20);
     static final Pose2D TARGET_2 = new Pose2D(DistanceUnit.MM, 120, 850, AngleUnit.DEGREES, -45);
-    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM, 400, 600, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM, 400, 620, AngleUnit.DEGREES, 0);
     static final Pose2D TARGET_4 = new Pose2D(DistanceUnit.MM, 120, 850, AngleUnit.DEGREES, -45);
-    static final Pose2D TARGET_5 = new Pose2D(DistanceUnit.MM, 120, 850, AngleUnit.DEGREES, -45);
+    static final Pose2D TARGET_5 = new Pose2D(DistanceUnit.MM, 400, 880, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_6 = new Pose2D(DistanceUnit.MM, 120, 850, AngleUnit.DEGREES, -45);
+    static final Pose2D TARGET_7 = new Pose2D(DistanceUnit.MM, 890, 500, AngleUnit.DEGREES, 90);
+    static final Pose2D TARGET_8 = new Pose2D(DistanceUnit.MM, 120, 850, AngleUnit.DEGREES, -45);
+    static final Pose2D TARGET_3_1 = new Pose2D(DistanceUnit.MM, 480, 620, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_5_1 = new Pose2D(DistanceUnit.MM, 480, 880, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_7_1 = new Pose2D(DistanceUnit.MM, 890, 620, AngleUnit.DEGREES, 90);
 
 
     @Override
@@ -109,7 +121,8 @@ public class FinalAuto extends LinearOpMode {
         // Wait for the game to start (driver presses START)
         waitForStart();
         resetRuntime();
-
+        servoPinceL.setPower(0);
+        servoPinceR.setPower(0);
         while (opModeIsActive()) {
             odo.update();
 
@@ -150,6 +163,17 @@ public class FinalAuto extends LinearOpMode {
                         armMotor.setPower(-1.0);
                         armOut = true;
                         timeArm = runtime.time();
+                        readyToGo = true;
+                        newTarget = 31;
+                        sleep(500);
+
+                    }
+
+                    break;
+                case Drive_TO_TARGET_3_1:
+                    if (nav.driveTo(odo.getPosition(), TARGET_3_1, 0.5, 0.0)) {
+                        telemetry.addLine("at position #3.1");
+                        stateMachine = StateMachine.AT_TARGET;
                         readyToGo = false;
                         newTarget = 4;
 
@@ -157,17 +181,87 @@ public class FinalAuto extends LinearOpMode {
 
                     break;
                 case DRIVE_TO_TARGET_4:
-                    if (nav.driveTo(odo.getPosition(), TARGET_4, 0.80, 0.5)) {
+                    if (nav.driveTo(odo.getPosition(), TARGET_4, 0.50, 0.5)) {
                         telemetry.addLine("at position #4");
-                        stateMachine = StateMachine.DRIVE_TO_TARGET_5;
+                        servoBucket.setPower(-1.0);
+                        bucketOut = true;
+                        timeBucket = runtime.time();
+                        stateMachine = StateMachine.AT_TARGET;
+                        readyToGo = false;
+                        newTarget = 5;
 
                     }
 
                     break;
                 case DRIVE_TO_TARGET_5:
-                    if (nav.driveTo(odo.getPosition(), TARGET_5, 0.6, 0.5)) {
-                        telemetry.addLine("There!");
+                    if (nav.driveTo(odo.getPosition(), TARGET_5, 0.5, 0.0)) {
+                        telemetry.addLine("at position #5");
                         stateMachine = StateMachine.AT_TARGET;
+                        armMotor.setPower(-1.0);
+                        armOut = true;
+                        timeArm = runtime.time();
+                        readyToGo = true;
+                        newTarget = 51;
+                        sleep(500);
+
+
+                    }
+                case DRIVE_TO_TARGET_5_1:
+                    if (nav.driveTo(odo.getPosition(), TARGET_5_1, 0.5, 0.5)) {
+                        telemetry.addLine("at position #5.1");
+                        stateMachine = StateMachine.AT_TARGET;
+                        readyToGo = false;
+                        newTarget = 6;
+
+                    }
+
+                    break;
+                case DRIVE_TO_TARGET_6:
+                    if (nav.driveTo(odo.getPosition(), TARGET_6, 0.5, 0.5)) {
+                        telemetry.addLine("at position #6");
+                        stateMachine = StateMachine.AT_TARGET;
+                        servoBucket.setPower(-1.0);
+                        bucketOut = true;
+                        timeBucket = runtime.time();
+                        readyToGo = false;
+                        newTarget = 7;
+
+                    }
+
+                    break;
+                case DRIVE_TO_TARGET_7:
+                    if (nav.driveTo(odo.getPosition(), TARGET_7, 0.6, 0.0)) {
+                        telemetry.addLine("at position #7");
+                        stateMachine = StateMachine.AT_TARGET;
+                        armMotor.setPower(-1.0);
+                        armOut = true;
+                        timeArm = runtime.time();
+                        readyToGo = true;
+                        newTarget = 71;
+                        sleep(500);
+
+                    }
+
+                    break;
+                case Drive_TO_TARGET_7_1:
+                    if (nav.driveTo(odo.getPosition(), TARGET_7_1, 0.6, 0.0)) {
+                        telemetry.addLine("at position #7.1");
+                        stateMachine = StateMachine.AT_TARGET;
+                        readyToGo = false;
+                        newTarget = 8;
+
+                    }
+
+                    break;
+                case DRIVE_TO_TARGET_8:
+                    if (nav.driveTo(odo.getPosition(), TARGET_8, 0.5, 0.5)) {
+                        telemetry.addLine("at position #8");
+                        stateMachine = StateMachine.AT_TARGET;
+                        servoBucket.setPower(-1.0);
+                        bucketOut = true;
+                        timeBucket = runtime.time();
+
+                        readyToGo = false;
                         newTarget = 0;
 
                     }
@@ -176,7 +270,7 @@ public class FinalAuto extends LinearOpMode {
             }
             elevatorMotor.setPower(ElevatorFunction.moveElevator(elevatorPower, elevatorIn, elevatorOut));
             if (timeBucket != 0.0) {
-                if (timeBucket + 2.0 <= runtime.time()) {
+                if (timeBucket + 1.3 <= runtime.time()) {
                     servoBucket.setPower(0);
                     timeBucket = 0.0;
                     if (bucketOut) {
@@ -189,46 +283,45 @@ public class FinalAuto extends LinearOpMode {
                 }
             }
             if (timeArm != 0.0) {
-                if (timeArm + 1.0 <= runtime.time()) {
+                if (timeArm + 0.4 <= runtime.time()) {
                     armMotor.setPower(0);
                     timeArm = 0.0;
                     if (armOut && !clawOut) {
+
                         timeClaw = runtime.time();
                         clawOut = true;
+                        sleep(5);
                         servoPinceR.setPower(-1);
                         sleep(5);
                         servoPinceL.setPower(1);
                         sleep(5);
+
                     }
-                    if (armOut && clawOut) {
+                    if (!armOut && clawOut) {
                         timeClaw = runtime.time();
                         clawOut = false;
-                        servoPinceR.setPower(1);
-                        sleep(5);
+                        sleep(500);
                         servoPinceL.setPower(-1);
                         sleep(5);
+                        servoPinceR.setPower(1);
+                        sleep(1000);
+                        readyToGo = true;
+                        elevatorPower = 1.0;
                     }
 
                 }
             }
             if (timeClaw != 0.0) {
-                if (timeClaw + 2.0 <= runtime.time()) {
+                if (timeClaw + 1.5 <= runtime.time()) {
                     servoPinceR.setPower(0);
                     servoPinceL.setPower(0);
                     timeClaw = 0.0;
-                    if (!armOut && clawOut) {
-                        clawOut = false;
-                        timeClaw = runtime.time();
-                        servoPinceR.setPower(1);
-                        servoPinceL.setPower(-1);
-                        readyToGo = true;
 
-                    } else if (!armOut && !clawOut) {
-                        readyToGo = true;
-                    } else if (clawOut && armOut) {
+
+                    if (clawOut && armOut) {
                         armOut = false;
                         timeArm = runtime.time();
-                        armMotor.setPower(1);
+                        armMotor.setPower(0.7);
                     }
                 }
             }
@@ -245,6 +338,30 @@ public class FinalAuto extends LinearOpMode {
                         break;
                     case 5:
                         stateMachine = StateMachine.DRIVE_TO_TARGET_5;
+
+                        break;
+                    case 6:
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_6;
+
+                        break;
+                    case 7:
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_7;
+
+                        break;
+                    case 8:
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_8;
+
+                        break;
+                    case 31:
+                        stateMachine = StateMachine.Drive_TO_TARGET_3_1;
+
+                        break;
+                    case 51:
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_5_1;
+
+                        break;
+                    case 71:
+                        stateMachine = StateMachine.Drive_TO_TARGET_7_1;
 
                         break;
                 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheNewAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheNewAuto.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-
+@Disabled
 @Autonomous
 public class TheNewAuto extends LinearOpMode {
 
